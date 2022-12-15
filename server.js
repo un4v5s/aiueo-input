e3ef8ee036c338b92fc991451d4a39f9bf99ced3
ooooo <un4v5s@gmail.com>
2022-12-15 23:55:56 +0900

    Modified for render

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// const viewsDir = path.join(__dirname, 'views')
-// app.use(express.static(viewsDir))
-const docsDir = path.join(__dirname, 'docs')
-app.use(express.static(docsDir))
-// app.use(express.static(path.join(__dirname, './public')))
-app.use(express.static(path.join(__dirname, './docs')))
+const viewsDir = path.join(__dirname, 'views')
+app.use(express.static(viewsDir))
+// const docsDir = path.join(__dirname, 'docs') // for gh pages
+// app.use(express.static(docsDir)) // for gh pages
+app.use(express.static(path.join(__dirname, './public')))
+// app.use(express.static(path.join(__dirname, './docs')))
 app.use(express.static(path.join(__dirname, './weights')))
 app.use(express.static(path.join(__dirname, './dist')))
 
-// app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
-app.get('/', (req, res) => res.sendFile(path.join(docsDir, 'index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
+// app.get('/', (req, res) => res.sendFile(path.join(docsDir, 'index.html'))) // for gh pages
 
 app.listen(3000, () => console.log('Listening on port 3000!'))
